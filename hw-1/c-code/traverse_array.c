a71f5047fd9e460d2e5e81c28c0aaab5166c4daf
Prabhvir Babra <theprabab@gmail.com>
2022-10-03 07:45:14 -0700

    Progress on C part of Handout

---
+++ hw-1/c-code/traverse_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stddef.h>
+
+int main () {
+
+  int n;
+  scanf("%d", &n);
+
+  int arr[n];
+  for(int i = 0; i < n; ++i)
+    scanf("%d", &arr[i]);
+
+  int l, r;
+  scanf("%d %d", &l, &r);
+
+  printf("%u %d", i, l);
+  for (size_t i = r; i >= l; --i) {
+    printf("%d ", arr[i]);
+  }
+
+  return 0;
+}
